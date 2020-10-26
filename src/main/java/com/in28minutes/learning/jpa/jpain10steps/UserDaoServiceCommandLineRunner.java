2d45371c2481ed81a423eb470e2286f2c273c0f6
Vinícius Ramaciotti <ramaciotti86@gmail.com>
2020-10-26 14:50:18 +0000

    First commit

---
+++ src/main/java/com/in28minutes/learning/jpa/jpain10steps/UserDaoServiceCommandLineRunner.java
@@ -0,0 +1,25 @@
+package com.in28minutes.learning.jpa.jpain10steps;
+
+import com.in28minutes.learning.jpa.jpain10steps.entity.User;
+import com.in28minutes.learning.jpa.jpain10steps.service.UserDAOService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class UserDaoServiceCommandLineRunner implements CommandLineRunner {
+
+    private static final Logger log = LoggerFactory.getLogger(UserDaoServiceCommandLineRunner.class);
+
+    @Autowired
+    private UserDAOService userDAOService;
+
+    @Override
+    public void run(String... args) throws Exception {
+        User user = new User("Jack", "Admin");
+        userDAOService.insert(user);
+        log.info("New user is created: " + user);
+    }
+}
---
+++ src/main/java/com/in28minutes/learning/jpa/jpain10steps/UserRepositoryCommandLineRunner.java
@@ -0,0 +1,35 @@
+package com.in28minutes.learning.jpa.jpain10steps;
+
+import com.in28minutes.learning.jpa.jpain10steps.entity.User;
+import com.in28minutes.learning.jpa.jpain10steps.service.UserRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.Optional;
+
+@Component
+public class UserRepositoryCommandLineRunner implements CommandLineRunner {
+
+    private static final Logger log = LoggerFactory.getLogger(UserRepositoryCommandLineRunner.class);
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public void run(String... args) throws Exception {
+        User user = new User("Jill", "Admin");
+        userRepository.save(user);
+        log.info("New user is created: " + user);
+
+        Optional<User> useruserWithIdOne = userRepository.findById(1L);
+        log.info("User is retrived: " + useruserWithIdOne);
+
+        List<User> users = userRepository.findAll();
+        log.info("All users : " + users);
+
+    }
+}
